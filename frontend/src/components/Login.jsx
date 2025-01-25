ac9535a1b626f7005919d7f4fafd6c9d50b50a0d
Arnav Shukla <arnavshukla@eduroam-campus-10-36-191-127.campus-dynamic.uwaterloo.ca>
2025-01-25 15:08:16 -0500

    navbar works

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -6,11 +6,7 @@ export default function Login() {
       <div>
         <h1>Login Page</h1>
         <p>Please login to continue.</p>
-        <p>Please login to continue.</p>
       </div>
-      <p className="bg-blue-500 text-white text-xl p-4">
-        Tailwind CSS is working!
-      </p>
     </>
   );
 }
