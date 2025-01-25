a3cb61a14a81797183b0e51adbb99f3be63e63c7
zen-karia <149807516+zen-karia@users.noreply.github.com>
2025-01-25 14:58:33 -0500

    made changes to server.js and added resume.js

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,41 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const resume = require('./resume');
+
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+
+
+// Test Route
+app.get("/test", (req, res) => {
+    res.send("SkillScan Backend working");
+});
+
+// Login Route
+
+
+// Home Page Route
+app.get("/home", (req, res) => {
     const skills = [
-        { id: 1, name: 'React', description: 'JavaScript library for building user interfaces' },
-        { id: 2, name: 'Node.js', description: 'JavaScript runtime built on Chrome\'s V8 JavaScript engine' },
+        { id: 1, name: 'Get your Resume Critiqued', description: 'Analyze your resume and get suggestions by AI' },
+        { id: 2, name: 'LinkedIn Profile improvement', description: 'Get suggestions for your linkedin profile' },
+        { id: 3, name: 'GitHub Profile Improvement', description: 'Get suggestions for your GitHub profile' },
+        { id: 4, name: 'Interview Preparation', description: 'Get questions and answers for your upcoming interview' },
+        { id: 5, name: 'Sign Out', description: 'Sign out from the application' }
     ];
     res.json(skills);
 });
+
+// Resume Route
+app.use('/improve-resume', resume);
 
 app.listen(PORT, () => {
     console.log(`Server is running on httpL//localhost:${PORT}`);
