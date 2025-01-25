87a8f5243f6264f0ffc0d380004eb7e942bef0ff
Arnav Shukla <arnavshukla@eduroam-campus-10-36-191-127.campus-dynamic.uwaterloo.ca>
2025-01-25 13:47:52 -0500

    made changes

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+export default function Login() {
+  return (
+    <>
+    <div>
+      <h1>Login Page</h1>
+      <p>Please login to continue.</p>
+    </div>
+    <div className="bg-blue-500 text-white text-xl p-4">
+  Tailwind CSS is working!
+</div>
+
+    </>
+  );
+}
