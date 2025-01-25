3f29a0b28554860e00454856ee83b8f19aa95d5d
Arnav Shukla <arnavshukla@eduroam-campus-10-36-191-127.campus-dynamic.uwaterloo.ca>
2025-01-25 13:47:52 -0500

    made changes

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>Welcome to the About page!</p>
+    </div>
+  )
+}
+
+
+
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Link, NavLink} from 'react-router-dom'
+
+export default function Navbar() {
+  return (
+    <nav className="bg-white shadow-md">
+      <div className="container mx-auto px-4 py-2 flex justify-between items-center">
+        <Link to="/" className="text-xl font-bold text-gray-800">Brand</Link>
+        <div className="flex space-x-4">
+          <NavLink to="/" className="text-gray-600 hover:text-gray-800">Home</NavLink>
+          <NavLink to="/about" className="text-gray-600 hover:text-gray-800">About</NavLink>
+          <NavLink to="/login" className="text-gray-600 hover:text-gray-800">Login</NavLink>
+        </div>
+      </div>
+    </nav>
+  )
+}
