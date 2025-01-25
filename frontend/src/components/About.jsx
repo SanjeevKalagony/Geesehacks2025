a6507eca89c03ac9919461317fc6071874e6ab2d
Arnav Shukla <arnavshukla@eduroam-campus-10-36-191-127.campus-dynamic.uwaterloo.ca>
2025-01-25 14:01:01 -0500

    made changes

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <div>
       <h1>About Page</h1>
-      <p>Welcome to the About page!</p>
+      <p className="text-lg font-semibold text-gray-700 mt-4">Welcome to the About page!</p>
     </div>
   )
 }
